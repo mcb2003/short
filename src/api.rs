be3522d8def3dcc39f62ddf8c3c7899c2a63a337
Michael Connor Buchan <mikey@blindcomputing.org>
2021-12-04 23:19:03 +0000

    Add an API endpoint to retrieve individual links

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,5 @@
 use tide::{Body, Error, Request, Response};
+use uuid::Uuid;
 
 use short::db::*;
 
@@ -6,6 +7,7 @@ pub fn install() -> tide::Server<()> {
     let mut app = tide::new();
         
 app.at("/links").get(all_links).post(new_link);
+app.at("/links/:id").get(view_link);
 
         app
 }
@@ -21,4 +23,14 @@ async fn new_link(mut req: Request<()>) -> tide::Result {
     let link = new_link.save().await?;
     let body = Body::from_json(&link)?;
     Ok(Response::builder(201).body(body).build())
+}
+
+async fn view_link(req: Request<()>) -> tide::Result {
+    let id: Uuid = req.param("id").unwrap().parse().map_err(|e| Error::new(400, e))?;
+    Ok(if let Some(link) = Link::by_id(id).await? {
+        let body = Body::from_json(&link)?;
+        Response::builder(200).body(body).build()
+    } else {
+        Response::new(404)
+    })
 }
